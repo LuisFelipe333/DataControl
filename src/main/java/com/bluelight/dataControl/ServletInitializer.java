c021068c34d88800a8e1e8291d8ee5faa4edf21a
LuisFelipe333 <luigifelipe333@gmail.com>
2023-03-10 20:06:26 -0600

    Creation Project

---
+++ src/main/java/com/bluelight/dataControl/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.bluelight.dataControl;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DataControlApplication.class);
+	}
+
+}
---
+++ src/test/java/com/bluelight/dataControl/DataControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bluelight.dataControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
